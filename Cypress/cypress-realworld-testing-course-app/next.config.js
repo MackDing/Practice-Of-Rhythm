5f8e26897a948abeb2d96b92b038ad33dd0c96ae
Blossom <MackDing@users.noreply.github.com>
2023-11-07 15:22:53 +0800

    add cypress-realworld-testing-course-app

---
+++ Cypress/cypress-realworld-testing-course-app/next.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  swcMinify: true,
+  images: {
+    domains: ["images.unsplash.com", "source.unsplash.com", "tailwindui.com"],
+  },
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          name: "[name].[ext]",
+        },
+      },
+    })
+    return config
+  },
+}
