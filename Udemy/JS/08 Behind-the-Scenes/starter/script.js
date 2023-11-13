9a7485fb115b334bac35b75a803d21f6796f8126
Blossom <MackDing@users.noreply.github.com>
2023-11-13 14:23:28 +0800

    ++

--- Udemy/JS/08 Behind-the-Scenes/starter/script.js
+++ Udemy/JS/08 Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -35,3 +36,29 @@ const firstName = 'Jonas'
 calcAge(1991)
 // console.log(age);
 // printAge();
+*/
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991
+
+
+// Function 
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+
+function addDecl(a, b) {
+  return a + b
+}
+
+const addExpr = function (a, b) {
+  return a + b
+}
+
+const addArrow = (a, b) => a + b
