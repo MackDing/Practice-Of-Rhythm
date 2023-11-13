261dee05abb139aa51151a5d557c3036feb2fcc3
Blossom <MackDing@users.noreply.github.com>
2023-11-13 09:50:18 +0800

    update

--- Udemy/JS/08-Behind-the-Scenes/starter/script.js
+++ Udemy/JS/08-Behind-the-Scenes/starter/script.js
@@ -1,22 +1,33 @@
 'use strict';
 
-const a = 'Jonas';
-first();
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
 
-function first() {
-  const b = 'Hello!';
-  second();
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
 
-  function second() {
-    const c = 'Hi!';
-    third();
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+      // Reassigning outer scope's variable
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
   }
+  printAge()
+  return age
 }
 
-
-function third() {
-  const d = 'Hey!';
-
-  console.log(d + c + b + a);
-}
-
+const firstName = 'Jonas'
+calcAge(1991)
+// console.log(age);
+// printAge();
