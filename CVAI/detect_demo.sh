2fd92350a881f049220835d1a754aa7964608a10
mack.ding <svip87@iCloud.com>
2024-02-29 11:08:12 +0800

    update README

--- CVAI/detect_demo.sh
+++ CVAI/detect_demo.sh
@@ -12,17 +12,11 @@ pip install -r requirements.txt
 
 python train.py --img 640 --batch 8 --epochs 100 --data coco128.yaml --weights yolov5s.pt
 ## Epoch: 术语"epoch"在深度学习中指的是遍历完一次所有数据集的过程。从0开始。
-##
 ## GPU_mem: 显示了当前使用的GPU内存。
-##
 ## box_loss, obj_loss, cls_loss: 这三者分别代表方框损失，目标损失，类别损失。方框损失指的是物体边框识别的准确性，目标损失和类别损失表示模型对目标对象的识别准确性。这些损失值都是越小越好。
-##
 ## Instances: 指的是当前epoch中，训练样本的数量。
-##
 ## Size: 这里的大小指的是输入模型的图片尺寸。
-##
 ## Class, Images, Instances: 分别表示类别名，测试集的图片数量和实例数量。
-##
 ## P, R, mAP50, mAP50-95: 这些是目标检测任务常用的评价指标，分别是精确度（Precision），召回率（Recall），平均精确度（mAP）在IoU（交并比）为0.5以及0.5到0.95时的值，值越大越好。
 
 // detect
