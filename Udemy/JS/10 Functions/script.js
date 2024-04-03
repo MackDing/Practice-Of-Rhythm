64902b697fe17a7f4c4533a3b72c6368ad21e8d2
mack.ding <svip87@iCloud.com>
2024-04-03 11:46:42 +0800

    ++

--- Udemy/JS/10 Functions/script.js
+++ Udemy/JS/10 Functions/script.js
@@ -497,3 +497,10 @@ GOOD LUCK 😀
 })();
 
 // call, apply, bind, closure
+
+/* 
+三者都可以改变函数的this对象指向。
+三者第一个参数都是this要指向的对象，如果如果没有这个参数或参数为undefined或null，则默认指向全局window。
+三者都可以传参，但是apply是数组，而call是参数列表，且apply和call是一次性传入参数，而bind可以分为多次传入。
+bind 是返回绑定this之后的函数，便于稍后调用；apply 、call 则是立即执行 。 
+*/
