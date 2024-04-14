2badcb625b96845fe84e882a73c8f68c8e555be5
Blossom <MackDing@users.noreply.github.com>
2024-04-14 21:41:30 +0800

    ++

--- Udemy/JS/14 OOP/script.js
+++ Udemy/JS/14 OOP/script.js
@@ -1,1 +1,55 @@
-'use strict';
+"use strict";
+
+const Person = function (firstName, birthYear) {
+  console.log(this);
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never to this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, is = {}
+// 3. {} linked to prototype
+// 4. function automatically turn {}
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("jack", 1975);
+console.log(matilda, jack);
+
+const jay = "jay";
+
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty("firstName"));
+console.log(jonas.hasOwnProperty("species"));
+
+
