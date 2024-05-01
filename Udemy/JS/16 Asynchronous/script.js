e8787988a94b51b4d13b8602d500e1bc865e3671
Blossom <MackDing@users.noreply.github.com>
2024-05-01 14:58:28 +0800

    ++

--- Udemy/JS/16 Asynchronous/script.js
+++ Udemy/JS/16 Asynchronous/script.js
@@ -357,62 +357,49 @@ console.log(x);
 
 // console.log('Getting position');
 
+<<<<<<< Updated upstream
 // getPosition().then(pos => console.log(pos));
 /* 
-const whereAmI = function () {
-  getPosition().then(pos => {
-    const { latitude: lat, longitude: lng } = pos.coords;
+=======
+const getPosition = function () {
+  return new Promise((resolve, reject) => {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
+};
+/* 
+getPosition().then(pos => console.log(pos));
+>>>>>>> Stashed changes
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
 
-  const apiUrl = `https://geocode.xyz/${lat},${lng}?geoit=json`;
-
-  if (typeof lat !== 'number' || isNaN(lat)) {
-    throw new Error('Invalid latitude');
-  }
-  if (typeof lng !== 'number' || isNaN(lng)) {
-    throw new Error('Invalid longitude');
-  }
-
-  return fetch(apiUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Request failed with status code ${response.status}`);
-      }
-      return response.json();
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
     })
     .then(data => {
-      if (!data) {
-        throw new Error('Data is null');
-      }
-      if (!data.city || !data.country) {
-        throw new Error('Missing city or country');
-      }
-
+      console.log(data);
       console.log(`You are in ${data.city}, ${data.country}`);
 
       return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Request failed with status code ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (!data || data.length !== 1) {
-        throw new Error('Invalid data from countries API');
-      }
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
 
-      renderCountry(data[0]);
+      return res.json();
     })
-    .catch(error => {
-      console.error('Error:', error);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💥`));
 };
 
+<<<<<<< Updated upstream
 btn.addEventListener('click', whereAmI);
  */
 
@@ -464,3 +451,81 @@ console.log('FIRST');
 // } catch (error) {
 //   alert(error.message);
 // }
+=======
+btn.addEventListener('click', whereAmI); */
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Consume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+/* 
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.log(err));
+ */
+
+const whereAmI = async function (country) {
+  
+};
+>>>>>>> Stashed changes
