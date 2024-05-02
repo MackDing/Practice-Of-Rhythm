44948826018222a72cb148981d685606575fbddf
mack.ding <svip87@iCloud.com>
2024-05-02 18:27:37 +0800

    ++272

---
+++ Udemy/JS/17 Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,22 @@
+// Importing module
+// import './shoppingCart.js';
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log('Importing module');
+// console.log(shoppingCart);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apple', 4);
+
+console.log(cart);
