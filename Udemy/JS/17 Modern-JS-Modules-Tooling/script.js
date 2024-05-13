2fead89b0d02c9491c06388c832df4ba7842b828
mack.ding <svip87@icloud.com>
2024-05-13 16:43:34 +0800

    +57

--- Udemy/JS/17 Modern-JS-Modules-Tooling/script.js
+++ Udemy/JS/17 Modern-JS-Modules-Tooling/script.js
@@ -36,7 +36,7 @@ console.log(cart);
 
 // boy1
 // 丁语棠 丁语程/辰/琛/宸 丁语凡 丁语行/航 丁语希/熙/熹
-
+/* 
 async function getLastPost() {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
@@ -52,3 +52,34 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+ */
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 37;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
