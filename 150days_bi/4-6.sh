2cf3cd57b685a4fb28ca5d2519c46abd50a3fec8
mack.ding <svip87@iCloud.com>
2024-07-17 16:44:57 +0800

    update readme.md

---
+++ 150days_bi/4-6.sh
@@ -0,0 +1,391 @@
+You did that for yourself ，not for me
+I did that for you
+You're welcome
+
+I know he's a bit young
+A bit? When i met him, he was 8
+
+Ugh! I hate you, andre
+
+Look at you , all handsome
+
+Hello!Come in, say hi to my meemaw
+Hi, nice to meet you
+Nice to meet you ,too
+
+I think she knows what i meant
+
+Sheldon, when i said i was fine
+I meant i didn't want to talk about it anymore
+
+I can't believe he has a girlfriend
+Are you happy now
+
+Uh, sweetie we've got to go
+No
+
+But he has to know how i feel
+
+Mon? Honey , i gotta ask you something .
+Now?
+
+Oh, that's a bad idea
+What do you mean "Bad idea"
+Well, kids need a mother
+
+Do you have any idea who i am
+
+Are you angry with me
+Why would you say that
+
+Dad
+Yeah
+Would you like to have a conversation
+Yeah,sure, absolutely
+All right,what would you like to talk about
+
+When i play, i play for me
+I don't need your charity
+Thank you
+
+What
+Who kissed you ?
+Gloria
+
+Excuse me,that's 50 bucks
+What
+Five oh dollars.
+
+Actually, we're stopping
+Because i want to get out of the car
+Dylan.
+Stop!
+
+Stop it. Manny,  he's fine
+Tell him you're fine
+I'm fine
+Okay. Good.get in the car.
+
+Get off me
+
+Please just get on the plane and come to New York, okay?
+You'll see you're the only person i want to be with
+
+I am so happy you came. You look amazing
+It was very kind for you to invite me
+I never go to places like this
+
+I know what kind of man i want to be
+
+He loves me not
+He loves me not
+He loves me not
+Okay, can we talk
+Actually, no, I'm kind of busy
+
+Do you have any idea where he might be?
+He's at her apartment
+
+Ty! Ty grant!
+Oh boy
+What are you doing in town?
+Visiting my folks
+Oh my goodness
+
+The point is, i hated it
+
+Look at you! You look great
+Do i? Thank you
+
+Are you serious
+You really want me to kill Carlo Castillo
+I-i want you to help him
+
+She's got a gun
+It's a letter, Lisa
+
+No, no , no, i can't wait that long.
+You gotta help me , please
+
+And i have one idea,  but it...it's kind of crazy
+I like crazy
+
+I was so angry with you last night
+
+So, Mary, how have you been
+Oh thank you so much for asking
+I've been well ,and you ?
+Very good
+Good
+
+But you have to know she loved you
+
+Wow, that's very nice. Mm-hmm.
+100 bucks and it's yours
+
+Uh what? You don't want the license?
+Out of the car.
+
+Do we do this too much
+I think so
+
+Yeah get off me
+Yeah
+
+A-actually, i was kind of just invited to a party
+Would it be okay if i go
+
+You really think so
+Yeah, i really do
+Thanks, chandler
+Get off
+
+But please don't be angry with Sam. It's not his fault.
+
+I think I've got a good idea
+
+Do you want me to cry
+Is that what you want
+Do you want to see me cry
+Sir! No sir!
+
+Oh man! I want that place so much
+
+Um,so ,w-what are you doing in town
+Tell her she's got to come
+Oh,no,no, you don't have to
+No,  i kind of think i have to
+
+Okay now jump
+What kind of video is this
+You know what? I've got to go to work. I'm sorry
+Okay,thanks!
+
+That was very kind of you
+I didn't do so much
+
+Hey, Bert, I'm looking for penny
+She didn't get off the bus
+That's 'cause she didn't get on
+
+Hello Rachel
+Get off
+
+Okay , everybody get out of the car
+
+Well...all right
+I'll give you 25 bucks
+And you knew about this
+
+Okay, okay. I will give you 20 bucks
+If you get out of this car right now. Hey
+I have no idea what you just said
+Dude, we gotta talk.
+Okay
+It's Mrs.Bigsby. She has a gun
+What?
+
+Hey, dinosaur guy! Look at you, so sad
+Come dance
+ That's okay thanks
+
+But
+I was invited. I-I'm the new member
+
+I'm sorry. I meant that-
+No, i know what you meant
+
+Well, it's kind of a surprise for griffin
+
+What kind of soup do you want
+
+We get off the train at the next stop in Oxnard
+We then take the 1:13 train back to union station
+
+Oh, what the hell?  Get off me
+
+What's this?
+Eight hundred and twelve bucks
+Well, maybe i don't need your money
+Wait wait i said maybe
+Uh, penny is angry at Leonard, Leonard's angry at me and penny.
+I'm angry at Leonard and Amy
+ Raj is angry at Howard
+
+Good morning
+Do you have any idea what time it is
+Of course i do
+
+Ay, poor thing
+She has to take the bus
+
+Actually, i have to go to work
+But congratulations Bree.  It's beauty
+
+We don't have a choice
+He's got to stay here
+
+Dad, i really gotta get home
+I know, i know, you gotta do homework
+
+You know Marcel
+Well
+I kind of...kind of lost him
+Jokes
+What kind of jokes
+It's very kind of you to help me like this
+Oh,  here ,sit
+And so
+I'm gonna get on this spaceship
+
+Let's go. Get in the car
+Where are we going
+
+Hey. I'll give you 50 bucks not to do this
+
+Did you kill it
+Yep
+Are you sure
+You tell me
+
+And now he's just so angry at me
+That i don't think he'll let me help him
+
+Do you have any idea where he is
+
+I gotta go say hi to her
+Okay,  we're doing this
+
+I need to talk to you
+Sure ,what's up
+Oh, sorry, i meant chandler
+I know
+
+This is my home, Catherine, and i want you to leave
+
+I wanted a brother when i was 10
+Not a 10-year-old brother
+
+Everyone hated us
+Well,  you know high school
+
+When i met your dad, i was fun, too
+
+Surprise!
+I invited him to dinner
+What
+
+Come on ,gabby
+It's a small town. People talk
+
+I've waited a year for this day
+Thanks but i gotta go to work
+Can you not look at me when i say this
+I hated that so much
+
+When i first met you
+I didn't like you
+
+Why have we waited so long to do this
+Because you never invited us
+
+Don't tell her i'm here
+Don't eat that
+
+They're in town for the night . They want to go to dinner
+No
+
+She hated that name
+
+Oh, my god. Did she get off the plane
+Did she get off the plane
+I got off the plane
+
+That is so kind of you
+Yeah it is
+
+We should get on the horse
+
+It's the sexiest place in town
+We have to get home
+
+See? It's kind of fun
+
+Are you really that kind of guy
+No
+
+You're so kind
+
+Phoebe, is everything okay
+Actually, no, no . You have to get off the plane
+
+Get in the car
+Go go go
+Mr.Treeger?
+Here is 50 bucks. Merry Christmas
+Tickets are 11 bucks
+I didn't. I wanted you to come
+Did you say something
+I'm not angry at you
+I have no idea what you're talking about
+Honey , we gotta go
+She's got to go back to London
+You've got to go home
+But i like it here
+Uh, i-i don't think that's what she meant.
+Is it？
+I have a date
+Phil, i just want you to know I'm sorry
+When i first met you, you know what i said to chandler
+I knew you would think so
+You don't need that right now
+She invited us to,uh, a party
+You come to town and you don't tell me
+I hated that town
+Everybody was so poor. Everybody was Mexican
+She's in town for a week
+And i hated that guy
+Oh , i think so
+I can understand that
+And when i dance, I look like this
+I don't hate you
+I just want you to be happy, and you will be happy
+I want you to meet somebody
+Oh, sorry. I knew what he meant
+I've got a date
+What
+He's got a girlfriend
+Come on. We have to go now
+All right . I've got to go
+I've got to go to work, I'm sorry .
+Okay，thanks
+You've got to tell her how you feel
+She's got to eat
+I got to go
+Are you all right
+We gotta call 9-1-1
+I don't think that's a very good idea
+I'm sorry.
+Don't be. I needed to remember this is a bad idea. Thank you.
+So , you have no idea where Haley is right now
+Do you have any idea who that is
+I've got to see you
+I've got to get off this plane
+I'm not angry with you
+I've missed you
+Your mother loved you so much, Zachary
+That was great. The kids loved you
+Excuse me, i wanted you to help
+Here's two dollars
+Please  
+I'll give you a dollar
+Mom, hurry ,get out of the car
+He'll be fine. Get in
+Yes,sir
+All right. Uh,Lily, let's..let's let's get on the bike
+Carefully, carefully
+You're very kind darling
+What kind of pills
+Sleeping pills
+I'm kind of hungry, mom
+Oh, it was kind of like a birthday party
