8b744e480752f88b850c9b8ff2aa564ab91cd1df
mack.ding <svip87@iCloud.com>
2024-07-29 15:33:14 +0800

    update 150days

---
+++ 150days_bi/10-12.sh
@@ -0,0 +1,275 @@
+
+Great,now my chest hurts.-What
+My chest hurts
+Mom,I've given this a lot of thought
+Okay,I have a question
+Yes,Penny
+You don't even like people touching you
+How are you going to have sex
+Okay,look,Ross,I realize my father is difficult
+But that's why you have got to be the bigger man here
+Look,sweetie,I could be the bigger man
+I could be the biggest man
+I could be a big,huge,giant man
+and it still wouldn't make any difference
+I am smart,I'm capable,
+and I can make a difference
+You're thinking about this way too much
+Just tell him and get it over with
+I'm s... Uh...C-Can we help you?
+ Name's Murray Clark
+I grew up in this house
+Oh!hi!-Oh！
+Well,my name is Cameron.This is my husband,Mitchell
+Well,that's funny
+She grew up without her dad
+Don't treat me like a child,treat me like a colleague.
+It was awful
+All weekend,I was treated like a plus one
+a second class citizen,a--
+A wife!
+I love san francisco
+I wish I was going with you
+I understand your envy
+As you know,I'm a lawyer
+Well,count me out
+What?why?
+Come on,I'm smart,
+I have a good job and I have only three percent body fat
+It's true
+I've seen him at the beach
+He's like a human chicken wing
+Ladies and gentlemen,I hold in my hand the final question
+"Howard，help me out of that tub
+I'm stuck again"
+I think it's time for you to forget about Rose
+move on with your life
+Okay,fine,fine,all right
+You want to win by cheating,go ahead,all right
+It's been 10 years,and I can't move on from you
+I'm sorry,I've been doing this for 7 years
+Trust me,you have to have eyes in the back of your head
+It's because I planned ahead
+I finished my Christmas shopping weeks ago
+My tie is gone!My tie is gone!
+Shelly,you don't really need a tie
+I have to find my tie.I have to find my tie.
+Amy！You can't go out tonight
+She told it to me
+ February 14,1982
+I was 23 months old,it was Valentine's Day
+and Mom and Dad went out for dinner
+You were there.Don't you remember?
+Janice.Hi,Janice.
+Ok,here we go.I don't think we should go out anymore
+I went to that diner,and...
+I saw Rob kiss the waitress
+Did you ever see An Officer and a Gentleman
+Well,he's kinda like the guy I went to see that with
+Bree asked me to join her company
+Yeah?Hey,that's great
+You must be exhausted
+No,I'm not tired at all
+You have no idea how much this hurts
+I don't even like comic books
+Yeah,me neither
+This woman,was she wearing a lot of makeup?
+She was.
+How did you know that
+I don't know.-You're smart girl
+Try a little harder.-I said I don't know
+I think I can make a difference
+Now's not the time,Joey.You can yell at me tomorrow
+Wait,no,Pheebs.I'm not gonna yell at you
+All right,let's just get this stupid game over with
+Great.I'll go first.-Okay
+Because I grew up with a crazy mom
+and two sisters to take care of
+did i ever tell you about my father?
+you want me to treat you like an adult,you got it
+My grandmother still treats my dad like a child
+Look,I'm not asking you to believe what I believe
+I'm just asking you to think about what you believe
+Well,I really envy your relationship
+Manny...as you know,
+your mother and Jorge had a relationship
+Shut up.I'm counting pills
+What about that girl last year at comic- con
+Doesn't count
+Why not？
+Oh，my God.This doesn't count.Okay?
+The interview was over
+That was the real Chandler Being in there
+This is just some crazy guy out in the hall
+I mean I'm probably 98% happy and maybe 2% jealous
+Alright.Final lesson...always be willing to walk away
+I do not want you to give me a fake grade
+It doesn't mean anything if you're lying
+but,I was kind of hoping you could change my grade
+Sometimes in life,you just get stuck
+I think you'd be great at it.Really.
+What?
+He said you were a coal digger
+Okay.I think we can move on
+Well,uh,the thing is,the date didn't go that well
+Oh,too bad
+I guess the thing to do now is just pick yourself up
+Dust yourself off,forget it and move on
+I can't do that
+Why the hell not
+You can't ask us,son.That's cheating.
+Good morning,Penny
+What,do you have eyes in the back of your head?
+Rise and shine,Papa
+The clouds are gone
+Well,maybe you and I go out for drinks
+You say you went out with him for, like,a year.
+Do we really need to talk about my old boyfriend now
+Well,my mom told me that Mr. Grove
+went to the hospital last night
+I just wanted to see how he was doing
+Okay and I can also speak a little French
+Well,he's going to Europe for the summer
+and he wants me to join him
+Did you just join this family
+The kid's an oddball
+Tom and I went out once
+What's going on?-Chandler's gone again
+You want to finish this job quickly
+and move on to the next one,and it's easier
+to put the window there instead of where I want it
+Well,you were fired
+No
+I told that guy who answered your phone
+I didn't get that message
+So this doesn't count
+Anyway,I'll be in my office
+Baby,you're exhausted
+and you need to go to sleep right now
+Come with me.Come with me.
+You okay
+Something hurts
+In fact,I gave it a lot of thought,
+and I decided it was time for us to live together
+I'm trying
+Try harder
+Hi,honey.Is your brother around?I need to yell at him
+Of course he's around
+Can we just get this over with?I have a spray tan at 4:00
+“somewhere”?
+You don't know where your husband grew up
+Did you ever go to a school dance
+Come on.That's how you treat a maid,not your wife
+So...you take me to dinner
+treat me like a lady
+and bring me flowers
+Shelly,I am asking you to do this for me
+How I envy you
+let's start
+As you know,Max and I are part owners of the diner
+Huh？
+As you know,the school talent show is coming up
+Jenny's playing the violin.What's Juanita going to do
+Well,that's hard to say
+You did it twice?
+The first time didn't really count.You know,it was Barry
+Remember,show your work
+I'm afraid if I blink,my eyes will get stuck
+Oh,my God,you're cheating.-No
+That's thinking ahead
+It was here on my desk and now it's gone
+I have to go out for orange juice
+Yes,I understand
+I have to go out for orange juice
+Mitchell
+I have to go out for orange juice
+Come on,you think she should go out with me,don't you
+We went to her brother's restaurant
+Maybe I'll take her to that new French restaurant down the street
+Come on,let's join the party
+Oh,man,this hurts
+I've given it a lot of thought
+In fact...
+I bought this for you last night
+I hope it's okay
+Yeah,well,y'know maybe you just need to try a little harder
+She's just using you to yell at us
+Come on,let's just get this over with
+Let's get this over with.
+Did she ever say anything about my garden
+Oleg,come on,film the room
+I want our baby to know that I was treated like a queen
+You're asking me to keep a secret
+Yeah
+I envy you your childhood
+Rachel,if you want to go out with him,you can
+Wendy,I tried
+Well,try harder
+Don't yell at me
+I've got to go to the emergency room
+Now?
+Did I ever tell you about my first girlfriend in high school
+Karen Berberick?
+She loved you
+She treated me like trash
+Yep.Count me in.See you at the airport
+Oh,hold on!-Ohh!
+My ex-wife took 50% of my stuff
+and left me 100% of my kids
+Is that your final decision
+It is
+Very well
+Hi,can you help me
+I was writing an e-mail on the "a" key got stuck
+Now it's just going "aaa..."
+So,what'd he do？
+#（what’d=what did口语写法，并不规范）
+Did he cheat on a test?He's a cheater
+He did not cheat on a test
+See?This is why I ask you to think ahead,okay
+is everybody gone?
+Your mother was the last to leave
+Be best friends with you?Go out with you
+Have a baby with you?
+Okay...Well,I'm going out with a guy my friends all really like
+Well,if you went to the library
+why don't you have any books?
+I'm not just tired.I'm exhausted
+Everything hurts
+I get up in the morning
+to do this job I don't even like
+And I'm doing it just for the money
+and it's not even a lot of money
+I keep trying harder and harder
+and it doesn't even make a difference
+So if you're going to yell at me or punish me
+let's just get it over with
+Did I ever tell you I grew up poor
+My whole family was treated like trash
+So,one day I asked God to make me rich and beautiful
+So people would envy me
+Take one and pass it back
+Take one and pass it back
+As you all know,this test'll count for 25% of your final grade
+Be sure to show all your work
+If you get stuck on a question
+move on and come back to it at the end
+And I shouldn't have to say this,
+but if there's any cheating,I will see it
+I got eyes in the back of my head
+I got more eyes than a potato
+Ms.Ingram?
+Yeah,Sheldon
+I'm done
+What?
+I enjoyed it very much
+I'll take another if you have one
+No,I don't have another one
+J-Just read ahead in the textbook
+Oh,boy
+Where's your dad?His car's gone
+Uh,he and Renee went out to dinner
+He and Renee?
+Yeah.They went to that french restaurant dad likes.
+They said you could join them if you want
+Oh,I want
