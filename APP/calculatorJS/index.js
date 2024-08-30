872f19bf0801f9f1bcc79e16c71aeabab2c26e2d
Blossom <svip87@icloud.com>
2024-08-30 12:22:06 +0800

    add calculatorJS app

---
+++ APP/calculatorJS/index.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
