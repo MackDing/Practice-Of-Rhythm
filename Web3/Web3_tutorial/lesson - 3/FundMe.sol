603696fcf21a5de95c1a5b7ba594256d03571ec2
Blossom <svip87@icloud.com>
2024-10-08 00:32:40 +0800

    update web3

--- Web3/Web3_tutorial/lesson - 3/FundMe.sol
+++ Web3/Web3_tutorial/lesson - 3/FundMe.sol
@@ -8,25 +8,33 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 // 4. If the target value is not reached during the lock up period, investors will refund after the lock up period
 
 contract FundMe {
-    mapping(address => uint256) public funderToAmount;
+    mapping(address => uint256) public fundersToAmount;
 
-    uint256 constant MINIMUN_VALUE = 100 * 10 ** 18; //wei 
+    uint256 constant MINIMUN_VALUE = 100 * 10 ** 18; //wei to USD
 
     AggregatorV3Interface internal dataFeed;
 
     uint256 constant TARGET = 1000 * 10 ** 18;
 
-    address owner;
+    address public owner;
 
-    constructor() {
+    uint256 deploymentTimestamp; // star time
+    uint256 lockTime; // How long
+
+    constructor(uint256 _lockTime) {
         // sepolia testnet
-        dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        dataFeed = AggregatorV3Interface(0x1AC55d145062C48ddda47f1AB2C5b7B0129842d5);
         owner = msg.sender;
+
+        deploymentTimestamp = block.timestamp;
+        lockTime = _lockTime;
     }
 
     function fund() external  payable {
         require(convertEthToUSD(msg.value) >= MINIMUN_VALUE, "Send more ETH");
-        funderToAmount[msg.sender] = msg.value;
+        // require(block.timestamp < deploymentTimestamp + lockTime, "window is close");
+        fundersToAmount[msg.sender] = msg.value;
     }
 
     function getChainlinkDataFeedLatestAnswer() public view returns (int) {
@@ -49,16 +57,52 @@ contract FundMe {
             return ethAmount * ethPrice / (10 ** 8);
     }
 
-    function transferOwnership(address newOwner) public {
-        require(msg.sender == owner, "this function can only be called by owner");
+    function transferOwnership(address newOwner) public onlyOwner{
+        // require(msg.sender == owner, "this function can only be called by owner");
         owner = newOwner;
     }
 
 
-    function getFund() external view {
+    function getFund() external windowClose onlyOwner{
         require(convertEthToUSD(address(this).balance) >= TARGET, "Target is not reached");
-        require(msg.sender == owner, "this function can only be called by owner");
+        // require(msg.sender == owner, "this function can only be called by owner");
+        // require(block.timestamp >= deploymentTimestamp + lockTime, "window is not closed");
+
+        // transfer: transfer ETH and revert if tx failed
+
+
+        // payable(msg.sender).transfer(address(this).blance) 
+
+        // send: transfer ETH and return if failed
+        // bool success = payable(msg.sender).send(address(this).balance) 
+
+        // call: transfer ETH with data return value of function and bool
+        bool success;
+        (success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "transfer tx failed");
+        fundersToAmount[msg.sender] = 0;
 
     }
     
+    function refund() external windowClose{
+        require(convertEthToUSD(address(this).balance) < TARGET, "Target is reached");
+        // uint256 amount = fundersToAmount[msg.sender];
+        require(fundersToAmount[msg.sender] != 0, "there is no fund for you ");
+        // require(block.timestamp >= deploymentTimestamp + lockTime, "window is not closed");
+
+        bool success;
+        (success, ) = payable(msg.sender).call{value: fundersToAmount[msg.sender] }("");
+        require(success, "transfer tx failed");
+        fundersToAmount[msg.sender] = 0;
+
+    }
+    modifier windowClose() {
+        require(block.timestamp >= deploymentTimestamp + lockTime, "window is not close");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "this function can only be called by owner");
+        _;
+    }
 }
